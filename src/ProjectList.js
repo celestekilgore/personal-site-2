e99ef0c9a239c8768a6d6ff157fe2fc47237d254
Celeste Kilgore <celestekilgore@gmail.com>
2023-12-10 17:41:48 -0500

    added links to project repos

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -22,15 +22,15 @@ function ProjectList() {
                   </ul>
                 </p>
                 <div class="card-actions justify-end">
-                  <button class="btn btn-primary">Front End
+                  <a class="btn btn-primary" href="https://github.com/celestekilgore/friender-frontend" target="_blank">Frontend
                   <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
-                  </button>
-                  <button class="btn btn-primary">Back End
+                  </a>
+                  <a class="btn btn-primary" href="https://github.com/celestekilgore/friender-backend" target="_blank">Backend
                   <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
-                  </button>
-                  <button class="btn btn-primary">Demo
+                  </a>
+                  <a class="btn btn-primary" href="" target="_blank">Demo
                   <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
-                 </button>
+                 </a>
                 </div>
               </div>
             </div>
@@ -49,15 +49,15 @@ function ProjectList() {
                   </ul>
                 </p>
                 <div class="card-actions justify-end">
-                  <button class="btn btn-primary">Front End
+                  <a class="btn btn-primary" href="https://github.com/celestekilgore/jobly-frontend" target="_blank">Frontend
                   <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
-                  </button>
-                  <button class="btn btn-primary">Back End
+                  </a>
+                  <a class="btn btn-primary" href="https://github.com/celestekilgore/jobly-backend" target="_blank">Backend
                   <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
-                  </button>
-                  <button class="btn btn-primary">Demo
+                  </a>
+                  <a class="btn btn-primary" href="" target="_blank">Demo
                   <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
-                 </button>
+                 </a>
                 </div>
               </div>
             </div>
