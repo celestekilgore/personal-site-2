289001e210ecb413158c39eac33225088aeb4180
Celeste Kilgore <celestekilgore@gmail.com>
2023-12-21 11:36:53 -0500

    Added links and photos for Warbler and Jobly

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -26,7 +26,7 @@ function ProjectList() {
               </div>
             </div>
             <div className="card lg:card-side bg-base-100 shadow-xl m-7">
-              <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+              <figure><img src="jobly.png" alt="Album"/></figure>
               <div className="card-body">
                 <h2 className="card-title">Jobly</h2>
                 <p className="text-left">A job search application that allows users to search for jobs and companies.  Built with a React Frontend and a Express Backend.</p>
@@ -38,7 +38,7 @@ function ProjectList() {
                   <a className="btn btn-primary" href="https://github.com/celestekilgore/jobly-backend" target="_blank">Backend
                   <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
                   </a>
-                  <a className="btn btn-primary" href="" target="_blank">Demo
+                  <a className="btn btn-primary" href="https://job-ly.surge.sh/" target="_blank">Demo
                   <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
                  </a>
                 </div>
@@ -51,13 +51,10 @@ function ProjectList() {
                 <p className="text-left">A twitter clone that allows users to follow/unfollow other users, like/unlike user's warbles, and view a feed of warbles.</p>
                 <p className="text-left"><b>Built with: </b>Flask, Python, PostgreSQL, SQLAlchemy, Jinja, WTForms</p>
                 <div className="card-actions justify-end">
-                  <a className="btn btn-primary" href="https://github.com/celestekilgore/jobly-frontend" target="_blank">Frontend
+                  <a className="btn btn-primary" href="https://github.com/celestekilgore/flask-warbler" target="_blank">GitHub
                   <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
                   </a>
-                  <a className="btn btn-primary" href="https://github.com/celestekilgore/jobly-backend" target="_blank">Backend
-                  <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
-                  </a>
-                  <a className="btn btn-primary" href="" target="_blank">Demo
+                  <a className="btn btn-primary" href="https://warbling-warbler.onrender.com/" target="_blank">Demo
                   <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg>
                  </a>
                 </div>
