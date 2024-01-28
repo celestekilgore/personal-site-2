7dfad57819e38525c806ccc7838587d26ae9c1a7
Celeste Kilgore <celestekilgore@gmail.com>
2024-01-27 20:48:02 -0500

    made some small grammar tweaks

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -10,7 +10,7 @@ function ProjectList() {
               <figure><img src="friender.png" alt="Album" /></figure>
               <div className="card-body">
                 <h2 className="card-title">Friender</h2>
-                <p className="text-left">A social networking site that allows users to find new friends in their area! Built with a React Frontend and a Flask Backend.</p>
+                <p className="text-left">A Tinder-like social website that allows users to find new friends in their area! Built with a React Frontend and a Flask Backend.</p>
                 <p className="text-left"><b>Built with: </b>React, Flask, SQLAlchemy, PostgreSQL, AWS S3, Bootstrap</p>
                 <div className="card-actions justify-end">
                   <a className="btn btn-primary" href="https://github.com/celestekilgore/friender-frontend" target="_blank" rel="noreferrer">Frontend
