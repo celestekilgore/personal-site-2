bddc85662811903a461dac596ab5c26fff4a0192
Celeste Kilgore <celestekilgore@gmail.com>
2024-05-23 17:11:58 -0400

    Updating information

--- src/About.js
+++ src/About.js
@@ -4,10 +4,27 @@ function About() {
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold pb-3">About Me</h1>
-          <p className="py-3">I am a detail-oriented, thoughtful, and organized software engineer with a passion for developing user-friendly, intuitive web applications.</p>
-          <p className="py-3"> I have a background in Cognitive Science, specifically focused on decision-making. I now leverage this interdisciplinary background to make thoughtful design decisions and optimize UX.</p>
+          <p className="py-3">
+          Hi! My name is Celeste and I am a recent University of Michigan
+          graduate ready to kickstart my career. I majored in cognitive science
+          with a specialized focus on decision-making processes, and I minored
+          in quantitative methods in the social sciences.
+          </p>
+          <p className="py-3">
+          Late in my college career, I took a Python course
+          that ignited a passion for coding. I loved the puzzle-like nature of the
+          problems we faced, and how coding allows you to be both creative and
+          technical at the same time. Consequently, after graduating I proceeded
+          to complete a software engineering bootcamp to further pursue my
+          technical interests, from which I recently graduated.
+          </p>
+          <p className="py-3">
+          Since graduation, I have been searching for a role in which I can
+          leverage my varied experiences with cognitive science, data analysis,
+          and software development.
+          </p>
           {/* <p className="mb-4">I have a background in Cognitive Science, specifically focused on decision-making. I now leverage this interdisciplinary background to improve accessibility and optimize UX.</p> */}
-          <a className="btn btn-primary mt-3" href="resume.pdf" target="_blank">Download CV</a>
+          <a className="btn btn-primary mt-3" href="resume.pdf" target="_blank">Download Resume</a>
         </div>
       </div>
     </div>
